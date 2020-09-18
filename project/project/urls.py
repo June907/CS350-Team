af583c39fa00cd57605e5db8cfd71882f15f5831
June <a1150927469@gmail.com>
2020-09-18 14:59:35 -0600

    new change

--- project/project/urls.py
+++ project/project/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView,HomeDetailView
 
 urlpatterns= [
+    path('post/<int:pk>/',HomeDetailView.as_view(),name='post_detail'),
 
     path('',HomePageView.as_view(),name='home'),
 ]
--- project/project/views.py
+++ project/project/views.py
@@ -1,6 +1,11 @@
-from django.views.generic import TemplateView
-
-class HomePageView(TemplateView):
+from django.views.generic import ListView,DetailView
+from .models import Post
+class HomePageView(ListView):
+    model=Post
     template_name='home.html'
+
+class HomeDetailView(DetailView):
+    model=Post
+    template_name='post_detail.html'
 
 # Create your views here.
