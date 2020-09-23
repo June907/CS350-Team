95251d63681f83f838a37320d22130fd0968bc40
June907 <a1150927469@gmail.com>
2020-09-23 03:12:46 +0000

    today change

--- project/project/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import HomePageView,HomeDetailView
-
-urlpatterns= [
-    path('post/<int:pk>/',HomeDetailView.as_view(),name='post_detail'),
-
-    path('',HomePageView.as_view(),name='home'),
-]
