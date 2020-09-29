ec1a6ff0fc27bb92f3859e7611ef0b8d85976194
ewhalenkamp <70407222+ewhalenkamp@users.noreply.github.com>
2020-09-28 20:17:31 -0600

    Bootstrap Implementation
    Additionally changed class-based views to method-based views for simplicity for the time being, will change back come time to implement CRUD

--- project/project/urls.py
+++ project/project/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import HomePageView,HomeDetailView
+from . import views
+#from .views import HomePageView,HomeDetailView
 
 urlpatterns= [
-    path('post/<int:pk>/',HomeDetailView.as_view(),name='post_detail'),
-
-    path('',HomePageView.as_view(),name='home'),
+#    path('post/<int:pk>/',HomeDetailView.as_view(),name='post_detail'),
+#
+#    path('',HomePageView.as_view(),name='home'),
+    
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about')
 ]
--- project/project/views.py
+++ project/project/views.py
@@ -1,11 +1,55 @@
 from django.views.generic import ListView,DetailView
 from .models import Post
-class HomePageView(ListView):
-    model=Post
-    template_name='home.html'
+from django.shortcuts import render
 
-class HomeDetailView(DetailView):
-    model=Post
-    template_name='post_detail.html'
+users = [
+    {
+        'name': 'Erik Halenkamp',
+        'id': '00001',
+        'role': 'admin',
+        'date_joined': 'August 24 , 2020'
+    },
+    {
+        'name': 'Junwen Huang',
+        'id': '00002',
+        'role': 'admin',
+        'date_joined': 'August 24 , 2020'
+    },
+    {
+        'name': 'Yusuf Kortobi',
+        'id': '00003',
+        'role': 'admin',
+        'date_joined': 'August 24 , 2020'
+    },
+    {
+        'name': 'Asepha Shaeffer',
+        'id': '00004',
+        'role': 'admin',
+        'date_joined': 'August 24 , 2020'
+    }
+]
+
+def home(request):
+    context = {
+        'users': users
+    }
+    return render(request, 'home.html', context)
+
+def about(request):
+    context = {
+        'users': users
+    }
+    return render(request, 'about.html', context)
+
+#class HomePageView(ListView):
+#    model=Post
+#    template_name='home.html'
+#    context = {
+#        'users': users
+#    }
+#
+#class HomeDetailView(DetailView):
+#    model=Post
+#    template_name='post_detail.html'
 
 # Create your views here.
